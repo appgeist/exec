25fd70acbce09202a957939bcd516a665994788a
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2019-07-09 12:07:36 +0300

    Add jsdoc

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const { exec } = require('child_process');
 const COMPACT_REGEXP = /\s+/g;
 const compact = str => str.replace(COMPACT_REGEXP, ' ');
 
+/**
+ * Execute a shell command and return a Promise with the output string
+ *
+ * @param {string} command Command to execute
+ * @returns {Promise<string>} Command output
+ */
 module.exports = command =>
   new Promise((resolve, reject) => {
     exec(compact(command), (err, stdout, stderr) => {
