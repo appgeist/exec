29c89056c19e46df811f987c970e455d339e667c
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2019-09-30 13:48:27 +0300

    Update deps & linting rules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 const COMPACT_REGEXP = /\s+/g;
-const compact = str => str.replace(COMPACT_REGEXP, ' ');
+const compact = (str) => str.replace(COMPACT_REGEXP, ' ');
 
 /**
  * Execute a shell command and return a Promise with the output string
@@ -9,7 +9,7 @@ const compact = str => str.replace(COMPACT_REGEXP, ' ');
  * @param {string} command Command to execute
  * @returns {Promise<string>} Command output
  */
-module.exports = command =>
+module.exports = (command) =>
   new Promise((resolve, reject) => {
     exec(compact(command), (err, stdout, stderr) => {
       if (err) reject(err);
